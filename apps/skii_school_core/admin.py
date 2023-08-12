fa9b7d2237720b23783fdf8fb45c423d53c005e2
ANDRE Simon <simon@emencia.com>
2023-08-12 23:59:47 +0200

    some fix on skii core models

--- apps/skii_school_core/admin.py
+++ apps/skii_school_core/admin.py
@@ -31,5 +31,9 @@ class WorktimeRessourceAdmin(admin.ModelAdmin):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    fields = [("name", "state"), ("start", "stop"), "agent_invited"]
+    ordering = ["start", "stop", "state", "name"]
 
+    def save_model(self, request, obj, form, change):
+        obj.user_creator = request.user
+        super().save_model(request, obj, form, change)
--- apps/skii_school_core/models.py
+++ apps/skii_school_core/models.py
@@ -1,7 +1,8 @@
+from django.utils.timezone import now
 from django.db import models
 from decimal import Decimal as D
 
-
+from django.db.models.signals import pre_save
 from apps.account.forms.registration import User
 
 
@@ -49,10 +50,12 @@ class CurrencyRessource(models.Model):
     code = models.CharField(max_length=3, unique=True)
     created = models.DateTimeField("Created", auto_now_add=True)
     last_modified = models.DateTimeField("Last Modified", auto_now=True)
-    factor = models.DecimalField("Price factor against dollars", max_digits=5,
-                                 decimal_places=3, default=D(1.000))
-    quantity = models.DecimalField("Price factor against dollars", max_digits=12,
-                                   decimal_places=2, default=D(0.))
+    factor = models.DecimalField(
+        "Price factor against dollars", max_digits=5, decimal_places=3, default=D(1.000)
+    )
+    quantity = models.DecimalField(
+        "Price factor against dollars", max_digits=12, decimal_places=2, default=D(0.0)
+    )
 
     def __str__(self):
         return f"{self.code}: {self.quantity} * {self.factor} = {self.quantity * self.factor}"
@@ -66,8 +69,8 @@ class WorktimeRessource(models.Model):
     name = models.CharField(max_length=255, default="New work time ressource")
     created = models.DateTimeField("Created", auto_now_add=True)
     last_modified = models.DateTimeField("Last Modified", auto_now=True)
-    time_elasped = models.FloatField("Work time (seconds)", default=0.)
-    time_planned = models.FloatField("Time planned (seconds)", default=600.)
+    time_elasped = models.FloatField("Work time (seconds)", default=0.0)
+    time_planned = models.FloatField("Time planned (seconds)", default=600.0)
 
     def __str__(self):
         return f"{self.name}: {self.time_elasped} / {self.time_planned}"
@@ -79,7 +82,7 @@ EVENTS_STATE = (
     ("in_progress", "In progress"),
     ("cancelled", "Cancelled"),
     ("postponed", "Postponed in time"),
-    ("finished", "finished")
+    ("finished", "finished"),
 )
 
 
@@ -87,15 +90,16 @@ class Event(models.Model):
     class Meta:
         verbose_name = "Event"
         verbose_name_plural = "Event(s)"
+
     name = models.CharField(max_length=255, default="New events")
     state = models.CharField(max_length=128, choices=EVENTS_STATE, default="draft")
     user_creator = models.ForeignKey(User, on_delete=models.PROTECT)
-    start = models.DateTimeField("Start time", auto_now_add=True)
-    stop = models.DateTimeField("Stop time", auto_now_add=True)
+    start = models.DateTimeField("Start time", default=now)
+    stop = models.DateTimeField("Stop time", default=now)
     agent_invited = models.ManyToManyField(User, blank=True, related_name="events")
 
     def __str__(self):
-        return f"{self.name}: {self.state}"
+        return f"{self.name}: {self.state} Date {self.start} / {self.stop}"
 
 
 class Location(models.Model):

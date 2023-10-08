0c701be729dc8fc5b8bd7de3fa5b4e85c391c9ae
ANDRE Simon <simon@emencia.com>
2023-10-08 04:29:49 +0200

    [FIX] Use uppercase on geo constant and fix dict copy

--- skii/platform/constants.py
+++ skii/platform/constants.py
@@ -1,12 +1,13 @@
-latitude_config = {
+import copy
+LATITUDE_RANGE_CONFIG = {
     "left_digits": 3,
     "right_digits": 4,
     "positive": False,
     "min_value": -90,
     "max_value": 90,
 }
-longitude_config = latitude_config.copy()
-longitude_config.update(
+LONGITUDE_RANGE_CONFIG = copy.deepcopy(LATITUDE_RANGE_CONFIG)
+LONGITUDE_RANGE_CONFIG.update(
     {
         "min_value": -180,
         "max_value": 180,

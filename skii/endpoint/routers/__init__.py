1465c473bbff4780a6e8270ac5bca1a9bbb296b3
ANDRE Simon <simon@emencia.com>
2023-10-08 03:35:32 +0200

    [NEW] teacher_lessons endpoint on agenda route

--- skii/endpoint/routers/__init__.py
+++ skii/endpoint/routers/__init__.py
@@ -2,10 +2,12 @@ from .student import sub_route as route_student
 from .teacher import sub_route as route_teacher
 from .location import router as route_location
 from .lesson import router as route_lesson
+from .agenda import route_agenda
 
 __all__ = [
     route_location,
     route_student,
     route_teacher,
     route_lesson,
+    route_agenda,
 ]
--- skii/endpoint/schemas/response.py
+++ skii/endpoint/schemas/response.py
@@ -1,8 +1,10 @@
 from typing import List, Type
 
-from ninja import Schema
+from ninja import Schema, Field
 from django.db.models import Model
 
+from skii.platform.schemas.agent import TeacherContract
+from skii.platform.schemas.event import LessonContract
 
 SkiiRecordContract: Schema = Type[Model]
 SkiiListContract: Schema = List[Model]
@@ -12,8 +14,14 @@ class SkiiMsgContract(Schema):
     message: str
 
 
+class TeacherLessonContract(TeacherContract):
+    """ Return Teachers agents with lessons related."""
+    lessons: List[LessonContract] = Field(alias="lessonevent_set")
+
+
 __all__ = [
     SkiiRecordContract,
     SkiiListContract,
     SkiiMsgContract,
+    TeacherLessonContract,
 ]

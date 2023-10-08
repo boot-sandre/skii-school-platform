8735bff159418ad9ff3b98468b1a1978ef93cdbe
ANDRE Simon <simon@emencia.com>
2023-10-08 03:35:32 +0200

    [NEW] teacher_lessons endpoint on agenda route

--- skii/endpoint/routers/agenda.py
+++ skii/endpoint/routers/agenda.py
@@ -1,52 +1,33 @@
-from typing import Optional, List
-
 from django.shortcuts import get_object_or_404
-from ninja import Router
 from django.http import HttpRequest
 from django.contrib.auth import get_user_model
 
+from ninja import Router
+
 from apps.base.schemas import FormInvalidResponseContract
 
-from skii.platform.schemas.agent import StudentContract, TeacherContract, UserSchema
 from skii.platform.models.agent import TeacherAgent
-from skii.platform.schemas.event import LessonContract
-from skii.endpoint.schemas.response import SkiiListContract
+from skii.endpoint.schemas.response import TeacherLessonContract
+from skii.endpoint.schemas.identifier import IntStrUUID4
+
 
 UserModel = get_user_model()
 
 
 route_agenda = Router(tags=["skii", "agenda"])
 
-
-class TeacherLessonContract(TeacherContract):
-    user: UserSchema
-    id: Optional[int]
-    lessons: List[LessonContract] = []
+RouterModel = TeacherAgent
 
 
-@route_agenda.post(
-    path="/fetch_teacher_agenda/{record_pk}/",
+@route_agenda.get(
+    path="/teacher_lessons/{record_pk}/",
     response={
-        200: SkiiListContract,
+        200: TeacherLessonContract,
         422: FormInvalidResponseContract,
     },
 )
-def fetch_teacher_agenda(
-    request: HttpRequest, record_pk: int, payload: StudentContract
+def teacher_lessons(
+    request: HttpRequest, record_pk: IntStrUUID4
 ):
-    agent_payload = payload.dict()
-    user_payload = agent_payload.pop("user")
-    agent_obj = get_object_or_404(TeacherAgent, id=record_pk)
-    user_obj = get_object_or_404(UserModel, id=agent_obj.user.id)
-    for attr, value in agent_payload.items():
-        setattr(agent_obj, attr, value)
-    agent_obj.save()
-    for attr, value in user_payload.items():
-        setattr(user_obj, attr, value)
-    user_obj.save()
-    agent_obj.refresh_from_db()
-    agent_obj.user.refresh_from_db()
-    return dict(
-        count=int(bool(agent_obj)),
-        item=agent_obj,
-    )
+    agent = get_object_or_404(RouterModel, pk=record_pk)
+    return 200, agent

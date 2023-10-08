61c55a12069accfb9947072c9e5497aecf1351a1
ANDRE Simon <simon@emencia.com>
2023-10-08 11:08:50 +0200

    [NEW] use limit_choices_to on relation between agent and user

--- skii/platform/models/agent.py
+++ skii/platform/models/agent.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from skii.platform.entities import (
@@ -6,6 +8,7 @@ from skii.platform.entities import (
 
 
 class StudentAgent(AgentEntity):
+    """ Student needs to be related to an active user."""
     class Meta:
         verbose_name = _("Student")
         verbose_name_plural = _("Student(s)")
@@ -13,6 +16,11 @@ class StudentAgent(AgentEntity):
 
 
 class TeacherAgent(AgentEntity):
+    """ Teacher needs to be related to a staff user."""
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE,
+        limit_choices_to={"is_active": True, "is_staff": True})
+
     class Meta:
         verbose_name = _("Teacher")
         verbose_name_plural = _("Teacher(s)")

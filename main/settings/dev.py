58a795d8e3382e5f6f850bddf9a759fde21bd3ba
ANDRE Simon <simon@emencia.com>
2023-10-08 05:01:57 +0200

    [CHG] bump to version 0.4.1 and update documentation/makefile/logging

--- main/settings/dev.py
+++ main/settings/dev.py
@@ -1,6 +1,7 @@
 from .settings import *  # noqa
 
 DEBUG = True
+DJANGO_LOG_LEVEL = "DEBUG"
 
 EMAIL_FILE_PATH = BASE_DIR / "email"  # noqa
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
@@ -10,61 +11,66 @@ INSTALLED_APPS += [  # noqa
 ]
 
 
-DJANGO_LOG_LEVEL = os.environ.get(  # noqa
-    "DJANGO_LOG_LEVEL", default="DEBUG" if DEBUG else "INFO"
+DJANGO_LOG_LEVEL = os.environ.get(
+    "DJANGO_LOG_LEVEL",
+    default="DEBUG" if DEBUG else "INFO"
 )
-LOGGING.update(  # noqa
-    {
-        "root": {
-            "handlers": ["console"],
+LOGGING.update({
+    "root": {
+        "handlers": ["console"],
+        "level": DJANGO_LOG_LEVEL,
+    },
+    "loggers": {
+        "skii": {
             "level": DJANGO_LOG_LEVEL,
         },
-        "loggers": {
-            "skii": {
-                "level": DJANGO_LOG_LEVEL,
-            },
-            "django": {
-                "level": DJANGO_LOG_LEVEL,
-            },
-            "django.server": {
-                "handlers": ["console"],
-                "level": DJANGO_LOG_LEVEL,
-                "propagate": False,
-            },
-            "django.request": {
-                "handlers": ["console"],
-                "level": DJANGO_LOG_LEVEL,
-                "propagate": False,
-            },
-            "django.template": {
-                "level": DJANGO_LOG_LEVEL,
-                "propagate": True,
-            },
-            "django.db.backends": {
-                "level": "INFO",
-                "propagate": True,
-            },
-            "django.security": {
-                "level": DJANGO_LOG_LEVEL,
-                "propagate": True,
-            },
-            "django.utils.autoreload": {
-                "handlers": ["null"],
-                "level": "NOTSET",
-                "propagate": False,
-            },
-            # Divers python loggers
-            "py.warnings": {
-                "handlers": ["null"],
-                "level": "NOTSET",
-                "propagate": False,
-            },
-            "parso": {
-                "level": "INFO",
-                "propagate": True,
-            },
-            "faker.factory": {"level": "INFO", "propagate": True},
-            "factory.generate": {"level": "INFO", "propagate": True},
+        "django": {
+            "level": DJANGO_LOG_LEVEL,
         },
-    }
-)
+        "django.server": {
+            "handlers": ["console"],
+            "level": DJANGO_LOG_LEVEL,
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": DJANGO_LOG_LEVEL,
+            "propagate": False,
+        },
+        "django.template": {
+            "level": DJANGO_LOG_LEVEL,
+            "propagate": True,
+        },
+        "django.db.backends": {
+            "level": "INFO",
+            "propagate": True,
+        },
+        "django.security": {
+            "level": DJANGO_LOG_LEVEL,
+            "propagate": True,
+        },
+        "django.utils.autoreload": {
+            "handlers": ["null"],
+            "level": "NOTSET",
+            "propagate": False,
+        },
+        # Divers python loggers
+        "py.warnings": {
+            "handlers": ["null"],
+            "level": "NOTSET",
+            "propagate": False,
+        },
+        "parso": {
+            "level": "INFO",
+            "propagate": True,
+        },
+        "faker.factory": {
+            "level": "INFO",
+            "propagate": True
+        },
+        "factory.generate": {
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+})

697927da9ad978a8f4f4c157dfe66130b73dfbb5
ANDRE Simon <simon@emencia.com>
2023-10-20 07:38:46 +0200

    Integrate license to all skii/ python files

--- skii/platform/controllers.py
+++ skii/platform/controllers.py
@@ -1,3 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# Copyright © Simon ANDRÉ <simon@emencia.com> synw (https://github.com/synw/)
+# project: SkiiSchoolPlatform
+# github: https://github.com/boot-sandre/skii-school-platform/
+# template: https://github.com/synw/django-spaninja
 from datetime import datetime
 from typing import Iterable
 
